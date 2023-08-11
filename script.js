95f287a9b8b40f301bc051891f8166102e92d621
Vinita210803 <gvinita191@gmail.com>
2023-08-11 11:21:17 +0530

    Initial Setup

---
+++ script.js
@@ -0,0 +1,60 @@
+let clickRuns = 0, clickWickets = 0, clickBall = 0, clickWhiteBall=0
+function run1Click ()
+{
+    clickRuns=clickRuns+1;
+    document.querySelector('.total-clickRuns').textContent = clickRuns;
+    ballClick()
+}
+function run2Click()
+{
+    clickRuns=clickRuns+2;
+    document.querySelector('.total-clickRuns').textContent = clickRuns;
+    ballClick()
+}
+function run3Click()
+{
+    clickRuns=clickRuns+3;
+    document.querySelector('.total-clickRuns').textContent = clickRuns;
+    ballClick()
+}
+function run4Click()
+{
+    clickRuns=clickRuns+4;
+    document.querySelector('.total-clickRuns').textContent = clickRuns;
+    ballClick()
+}
+function run6Click()
+{
+    clickRuns=clickRuns+6;
+    document.querySelector('.total-clickRuns').textContent = clickRuns;
+    ballClick()
+}
+function wideBallClick()
+{
+    clickRuns=clickRuns+1;
+    document.querySelector('.total-clickRuns').textContent = clickRuns;
+}
+function wicketClick()
+{
+    document.getElementById("wickets").value = "wicket"
+    clickWickets=clickWickets+1;
+    document.querySelector('.total-clickWickets').textContent = clickWickets;
+    ballClick()
+}
+function noBallClick()
+{
+    // run1Click()
+    clickRuns=clickRuns+1;
+    alert("click corresponding runs obtained")
+    document.querySelector('.total-clickRuns').textContent = clickRuns;
+    clickBall=clickBall-1;
+    document.getElementById("wickets").value="runout";
+    
+}
+function ballClick()
+{
+    clickBall=clickBall+1;
+    document.querySelector('.total-clickBall').textContent = clickBall;
+}
+
+
