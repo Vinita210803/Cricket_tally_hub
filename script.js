a28f6aafdc6e6de911305251e50934377c110ef8
Vinita210803 <gvinita191@gmail.com>
2023-08-11 21:34:20 +0530

    11augsetup

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 let clickRuns = 0, clickWickets = 0, clickBall = 0, clickWhiteBall=0
+function run0Click ()
+{
+    clickRuns=clickRuns+0;
+    document.querySelector('.total-clickRuns').textContent = clickRuns;
+    ballClick()
+    document.getElementById("wickets").value = "wicket"
+}
 function run1Click ()
 {
     clickRuns=clickRuns+1;
     document.querySelector('.total-clickRuns').textContent = clickRuns;
     ballClick()
+    document.getElementById("wickets").value = "wicket"
 }
 function run2Click()
 {
     clickRuns=clickRuns+2;
     document.querySelector('.total-clickRuns').textContent = clickRuns;
     ballClick()
+    document.getElementById("wickets").value = "wicket"
 }
 function run3Click()
 {
     clickRuns=clickRuns+3;
     document.querySelector('.total-clickRuns').textContent = clickRuns;
     ballClick()
+    document.getElementById("wickets").value = "wicket"
 }
 function run4Click()
 {
     clickRuns=clickRuns+4;
     document.querySelector('.total-clickRuns').textContent = clickRuns;
     ballClick()
+    document.getElementById("wickets").value = "wicket"
 }
 function run6Click()
 {
     clickRuns=clickRuns+6;
     document.querySelector('.total-clickRuns').textContent = clickRuns;
     ballClick()
+    document.getElementById("wickets").value = "wicket"
 }
 function wideBallClick()
 {
@@ -36,7 +48,7 @@ function wideBallClick()
 }
 function wicketClick()
 {
-    document.getElementById("wickets").value = "wicket"
+    // document.getElementById("wickets").value = "wicket"
     clickWickets=clickWickets+1;
     document.querySelector('.total-clickWickets').textContent = clickWickets;
     ballClick()
